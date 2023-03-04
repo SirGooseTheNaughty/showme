fb2f8fb89406439eeb8a4ef2ab3863f7a640385d
Sergey Vorobyev <syvorobyev@gmail.com>
2023-03-04 16:13:37 +0400

    add visualization

--- app.js
+++ app.js
@@ -14,7 +14,24 @@ export const appComponent = {
         
     },
     methods: {
-        
+        filter: function() {
+            let filterText = '';
+            Object.entries(this.data).forEach(([key, filter]) => {
+                switch (filter.type) {
+                    case 'dropdown':
+                        filterText += `\n ${filter.title}: ${filter.value}`;
+                        break;
+                    case 'dots': 
+                        filterText += `\n ${filter.title}: ${filter.selected.join(', ')}`;
+                        break;
+                    case 'range': 
+                        filterText += `\n ${filter.title}: from ${filter.from} to ${filter.to}`;
+                        break;
+                }
+            });
+            console.log(filterText)
+            document.querySelector('.result').textContent = filterText;
+        }
     },
     computed: {
         
